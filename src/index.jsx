0e88e16ce1c8817794adc5116848bda00bcfbb96
Holly Tunison <hollytunison@gmail.com>
2022-01-16 15:21:16 -0500

    added store

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import { MainView } from './components/main-view/main-view';
 
@@ -25,6 +26,7 @@ class MyFlixApplication extends React.Component {
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
+const store = createStore(moviesApp, devToolsEnhancer());
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
